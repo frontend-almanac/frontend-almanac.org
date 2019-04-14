c8a8058f029fcec53479c29b10566fe7bfef50db
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2019-04-15 01:18:23 +0300

    jpg

--- components/OpenGraph/openGraph.js
+++ components/OpenGraph/openGraph.js
@@ -6,7 +6,7 @@ function OpenGraph(props) {
       <meta property="og:url" content={props.url} />
       <meta property="og:title" content={props.title} />
       <meta property="og:description" content={props.content} />
-      <meta property="og:image" content='https://frontend-almanac.org/static/logo-fb.png?v2' />
+      <meta property="og:image" content='https://frontend-almanac.org/static/logo-fb.jpg?v2' />
       
       
       <meta name="twitter:card" content="summary"></meta>
