fa901800e5f3b47a8f61bcb0ef137d2dbfbe13da
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2019-04-21 16:55:57 +0300

    sort order was changed

--- services/videoList.js
+++ services/videoList.js
@@ -13,8 +13,8 @@ class videoList {
   async fetch() {
     this.videos = await this.cacheFetch('https://raw.githubusercontent.com/frontend-almanac/frontend-almanac.org/master/list.csv?' + this.version);
     this.videos = csvToJSON(this.videos).sort((first, second) => {
-      if (first.year > second.year) return 1;
-      if (first.year < second.year) return -1;
+      if (first.year > second.year) return -1;
+      if (first.year < second.year) return 1;
       return 0;
     });
     return mergeConferenceInfo(this.videos, conferencies);
