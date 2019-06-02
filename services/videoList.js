8dc93e8dd242b497ef9a7a91ae8296d9e8468c49
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2019-06-02 22:57:38 +0300

    Zlit Tech

--- services/videoList.js
+++ services/videoList.js
@@ -8,7 +8,7 @@ class videoList {
   constructor() {
     this.videos = [];
     this.cacheFetch = cache(fetch);
-    this.version = 9;
+    this.version = 10;
   }
   async fetch() {
     this.videos = await this.cacheFetch('https://raw.githubusercontent.com/frontend-almanac/frontend-almanac.org/master/list.csv?' + this.version);
