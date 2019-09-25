30d9209d96a94c57f832c093609e6ea0412e9477
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2019-09-25 21:27:50 +0300

    Bump cache version up

--- services/videoList.js
+++ services/videoList.js
@@ -8,7 +8,7 @@ class videoList {
   constructor() {
     this.videos = [];
     this.cacheFetch = cache(fetch);
-    this.version = 11;
+    this.version = 12;
   }
   async fetch() {
     this.videos = await this.cacheFetch('https://raw.githubusercontent.com/frontend-almanac/frontend-almanac.org/master/list.csv?' + this.version);
